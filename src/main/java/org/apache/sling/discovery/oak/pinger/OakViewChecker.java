dafc2909d4b9405e641fe5bf1e837ec7a9a80da9
Ashok Pelluru <ashokmca07@gmail.com>
2022-04-29 13:39:51 +0200

    sling discovery bundles: fixing sonar cloud issues

--- src/main/java/org/apache/sling/discovery/oak/pinger/OakViewChecker.java
+++ src/main/java/org/apache/sling/discovery/oak/pinger/OakViewChecker.java
@@ -39,9 +39,6 @@ import org.apache.sling.discovery.oak.OakDiscoveryService;
 import org.apache.sling.settings.SlingSettingsService;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.http.HttpService;
 
 /**
  * The OakViewChecker is taking care of checking the oak discovery-lite
@@ -54,9 +51,6 @@ import org.osgi.service.http.HttpService;
  * discovery.base
  */
 @Component( service = OakViewChecker.class)
-/*@Reference(service=HttpService.class,
-        cardinality=ReferenceCardinality.MULTIPLE,
-        policy=ReferencePolicy.DYNAMIC)*/
 public class OakViewChecker extends BaseViewChecker {
 
     @Reference
